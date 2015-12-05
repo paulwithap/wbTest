5d49e2667990a3257caa1a3411d5034414754aef
Paul Jaworski <pjaworski27@gmail.com>
2015-12-04 16:16:45 -0800

    fix missing dependencies, build, remove dist from gitignore

--- test/spec/controllers/products.js
+++ test/spec/controllers/products.js
@@ -17,7 +17,7 @@ describe('Controller: ProductsCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(ProductsCtrl.awesomeThings.length).toBe(3);
-  });
+  // it('should attach a list of awesomeThings to the scope', function () {
+  //   expect(ProductsCtrl.awesomeThings.length).toBe(3);
+  // });
 });
